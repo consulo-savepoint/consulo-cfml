1009c68498e4cd0294f45c066522cf7d8b71fad7
VISTALL <vistall.valeriy@gmail.com>
2014-02-09 01:25:57 +0200

    compilation fixed

--- src/com/intellij/coldFusion/UI/editorActions/structureView/CfmlStructureViewProvider.java
+++ src/com/intellij/coldFusion/UI/editorActions/structureView/CfmlStructureViewProvider.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.coldFusion.UI.editorActions.structureView;
 
+import org.jetbrains.annotations.NotNull;
 import com.intellij.ide.structureView.StructureView;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
@@ -22,29 +23,35 @@ import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.ide.structureView.impl.StructureViewComposite;
 import com.intellij.ide.structureView.impl.TemplateLanguageStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by Lera Nikolaenko
  * Date: 20.01.2009
  */
-public class CfmlStructureViewProvider implements PsiStructureViewFactory {
-  @Override
-  public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-    return new TemplateLanguageStructureViewBuilder(psiFile) {
-      @Override
-      protected StructureViewComposite.StructureViewDescriptor createMainView(final FileEditor fileEditor, final PsiFile mainFile) {
-        StructureView mainView = new TreeBasedStructureViewBuilder() {
-          @Override
-          @NotNull
-          public StructureViewModel createStructureViewModel() {
-            return new CfmlStructureViewModel(psiFile);
-          }
-        }.createStructureView(fileEditor, mainFile.getProject());
-        return new StructureViewComposite.StructureViewDescriptor("CFML View", mainView, mainFile.getFileType().getIcon());
-      }
-    };
-  }
+public class CfmlStructureViewProvider implements PsiStructureViewFactory
+{
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TemplateLanguageStructureViewBuilder(psiFile)
+		{
+			@Override
+			protected StructureViewComposite.StructureViewDescriptor createMainView(final FileEditor fileEditor, final PsiFile mainFile)
+			{
+				StructureView mainView = new TreeBasedStructureViewBuilder()
+				{
+					@Override
+					@NotNull
+					public StructureViewModel createStructureViewModel(Editor editor)
+					{
+						return new CfmlStructureViewModel(psiFile);
+					}
+				}.createStructureView(fileEditor, mainFile.getProject());
+				return new StructureViewComposite.StructureViewDescriptor("CFML View", mainView, mainFile.getFileType().getIcon());
+			}
+		};
+	}
 }
