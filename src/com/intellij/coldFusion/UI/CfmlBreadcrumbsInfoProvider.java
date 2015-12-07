68808e2c1f7a5b9db76e513adf5406a8e8fa675b
VISTALL <vistall.valeriy@gmail.com>
2015-12-07 17:53:47 +0300

    compilation

--- src/com/intellij/coldFusion/UI/CfmlBreadcrumbsInfoProvider.java
+++ src/com/intellij/coldFusion/UI/CfmlBreadcrumbsInfoProvider.java
@@ -16,43 +16,51 @@
 package com.intellij.coldFusion.UI;
 
 import org.jetbrains.annotations.NotNull;
+import org.mustbe.consulo.RequiredReadAction;
 import com.intellij.coldFusion.model.CfmlLanguage;
 import com.intellij.coldFusion.model.psi.CfmlTag;
 import com.intellij.lang.Language;
-import com.intellij.lang.html.HTMLLanguage;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.xml.XmlTag;
 import com.intellij.xml.breadcrumbs.BreadcrumbsInfoProvider;
 
 /**
  * Created by Lera Nikolaenko
  * Date: 09.02.2009
  */
-public class CfmlBreadcrumbsInfoProvider extends BreadcrumbsInfoProvider {
-  public Language[] getLanguages() {
-    return new Language[]{CfmlLanguage.INSTANCE, HTMLLanguage.INSTANCE};
-  }
+public class CfmlBreadcrumbsInfoProvider extends BreadcrumbsInfoProvider
+{
+	@NotNull
+	@Override
+	public Language getLanguage()
+	{
+		return CfmlLanguage.INSTANCE;
+	}
 
-  public boolean acceptElement(@NotNull final PsiElement e) {
-    return e instanceof CfmlTag || e instanceof XmlTag;
-  }
+	@RequiredReadAction
+	public boolean acceptElement(@NotNull final PsiElement e)
+	{
+		return e instanceof CfmlTag /*|| e instanceof XmlTag*/;
+	}
 
-  public PsiElement getParent(@NotNull final PsiElement e) {
-    return e instanceof CfmlTag ?
-           PsiTreeUtil.getParentOfType(e, CfmlTag.class) :
-           PsiTreeUtil.getParentOfType(e, XmlTag.class);
-  }
+	@RequiredReadAction
+	public PsiElement getParent(@NotNull final PsiElement e)
+	{
+		return e instanceof CfmlTag ? PsiTreeUtil.getParentOfType(e, CfmlTag.class) : null /*: PsiTreeUtil.getParentOfType(e, XmlTag.class)*/;
+	}
 
-  @NotNull
-  public String getElementInfo(@NotNull final PsiElement e) {
-    String result = e instanceof CfmlTag ? ((CfmlTag)e).getTagName() :
-                    e instanceof XmlTag ? ((XmlTag)e).getName() : "";
-    return result != null ? result : "";
-  }
+	@RequiredReadAction
+	@NotNull
+	public String getElementInfo(@NotNull final PsiElement e)
+	{
+		String result = e instanceof CfmlTag ? ((CfmlTag) e).getTagName() : null/*: e instanceof XmlTag ? ((XmlTag) e).getName() : ""*/;
+		return result != null ? result : "";
+	}
 
-  public String getElementTooltip(@NotNull final PsiElement e) {
-    return null;
-  }
+	@RequiredReadAction
+	public String getElementTooltip(@NotNull final PsiElement e)
+	{
+		return null;
+	}
 }
 
